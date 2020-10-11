c027a19f6bfed4dd00b71d29b6f51e9aae4ff7a1
xiangst <280304286@163.com>
2020-10-11 13:27:46 +0800

    chore: submit source

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # find-ip-address
+
+## How to use
+
+```
+$ npm install
+$ npm run start
+```
+
+## Get ip address
+
+```
+# ----------------------------------------------
+# ----- Copy This To Modify /etc/host File -----
+# ----------------------------------------------
+140.82.112.4    github.com
+185.199.108.153 assets-cdn.github.com
+199.232.69.194  github.global.ssl.fastly.net
+185.199.108.154 github.githubassets.com
+199.232.68.133  avatars0.githubusercontent.com
+199.232.68.133  avatars1.githubusercontent.com
+199.232.68.133  avatars2.githubusercontent.com
+199.232.68.133  avatars3.githubusercontent.com
+199.232.68.133  avatars4.githubusercontent.com
+140.82.112.5    api.github.com
+140.82.114.10   codeload.github.com
+199.232.68.133  raw.githubusercontent.com
+# ----------------------------------------------
+```
---
+++ src/index.js
@@ -0,0 +1,84 @@
+const puppeteer = require('puppeteer');
+
+const hostList = [
+    "github.com",
+    "assets-cdn.github.com",
+    "github.global.ssl.fastly.net",
+    "github.githubassets.com",
+    "avatars0.githubusercontent.com",
+    "avatars1.githubusercontent.com",
+    "avatars2.githubusercontent.com",
+    "avatars3.githubusercontent.com",
+    "avatars4.githubusercontent.com",
+    "api.github.com",
+    "codeload.github.com",
+    "raw.githubusercontent.com",
+];
+
+(async () => {
+    console.log('Start ...\n')
+
+    const promiseList = [];
+    const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true,
+        // headless: false,
+        // devtools: true,
+        args: [
+            '--no-sandbox',
+            '--enable-strict-mixed-content-checking',
+            '--unsafely-treat-insecure-origin-as-secure'
+        ]
+    });
+    for (const host of hostList) {
+        promiseList.push(getIpAddress(browser, host))
+    }
+    const result = await Promise.all(promiseList)
+    format(result)
+    await browser.close()
+    console.log('\nDONE!')
+})();
+
+function format(result) {
+    console.log(`# ----------------------------------------------`)
+    console.log(`# ----- Copy This To Modify /etc/host File -----`)
+    console.log(`# ----------------------------------------------`)
+    for (const info of result) {
+        if (info.ip) {
+            console.log(`${info.ip}${Array(17 - info.ip.length).join(' ')}${info.origin}`)
+        } else {
+            console.log(`# Error -> ${info.origin}`)
+        }
+    }
+    console.log(`# ----------------------------------------------`)
+}
+
+function getSearchUrl(origin) {
+    if (!/^http/.test(origin)) origin = `https://${origin}`
+    let urlInfo = new URL(origin)
+    let hostSubName = urlInfo.hostname.split('.').reverse().reduce((total, current) => {
+        return total.length < 2 ? total.push(current) : null, total
+    }, []).reverse().join('.')
+    if (hostSubName === urlInfo.hostname) {
+        return `https://${hostSubName}.ipaddress.com/`
+    }
+    return `https://${hostSubName}.ipaddress.com/${urlInfo.hostname}`
+}
+
+async function getIpAddress(browser, origin) {
+    const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+
+    try {
+        await page.goto(getSearchUrl(origin), {waitUntil: 'domcontentloaded'});
+        return {origin, ip: await getFromPage(page)};
+    } catch (e) {
+        return {origin, error: e.toString()};
+    }
+}
+
+
+async function getFromPage(page) {
+    return page.evaluate(() => {
+        return document.querySelector("#dnsinfo > tr:nth-child(1) > td:nth-child(3) > a").innerText
+    })
+}
